925f62e093eaee7f14ce28cfd7c58f9dc9a4cc36
marioantolienzh <50441834+marioantolienzh@users.noreply.github.com>
2022-02-09 00:36:21 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,12 @@
+from socket import *
 
+serverPort = ipp #from ss -t -l on terminal
+
+serverSocket = socket(AF_INET,SOCK_STREAM)
+serverSocket.bind((‘’,serverPort))
+serverSocket.listen(1)
+
+print('The server is ready to receive')
+
+while True:
+  connecitonSocket, addr = serverSocket.accept()
