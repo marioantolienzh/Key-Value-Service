410a67e703c4e61830993594df44fc92014b919e
marioantolienzh <50441834+marioantolienzh@users.noreply.github.com>
2022-02-09 00:33:27 -0500

    Update client.py

--- client.py
+++ client.py
@@ -8,10 +8,20 @@ print('Welcome to the keyValue Service Client\n')
 token = 0
 
 while(token == 0):
-    inp = input('Enter the IP address of Hostname of the server: ')
-
+    serverName = raw_input('Enter the IP address of Hostname of the server: ')
+    serverPort = raw_input('Enter the port of the server: ')
     try:
         print('Please wait while I connect you...\n')
+        
+        clientSocket = socket(AF_INET, SOCK_STREAM)
+        clientSocket.connect((serverName,serverPort))
+        sentence = raw_input(‘Input lowercase sentence:’)
+        
+        clientSocket.send(sentence.encode())
+        modifiedSentence = clientSocket.recv(1024)
+        print (‘From Server:’, modifiedSentence.decode())
+        
+        clientSocket.close()
         token = 1
         break
     except ValueError:
