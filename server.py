70d5aed595c838fb91abcab01081b8743f3cbb37
marioantolienzh <50441834+marioantolienzh@users.noreply.github.com>
2022-02-10 18:16:27 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
+import sys
 import socket
+import time
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
@@ -9,8 +11,18 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     conn, addr = s.accept()
     with conn:
         print('Connected by', addr)
+        counter = 0
         while True:
             data = conn.recv(1024)
-            if not data:
+            print(data)
+            if data:
+                if(counter==0):
+                    conn.sendall(b'Welcome to the keyValue Service')
+                    counter+=1
+                if(counter==1):
+                    time.sleep(1)
+                    conn.sendall(b'KeyValue Service> ')
+                    counter+=1
+            else:
+                print('Ending connection')
                 break
-            conn.sendall(data)
